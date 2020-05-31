635b65a333d7dc89696443c79e7a616dbbc843b9
Suraj kc <isurajkc@gmail.com>
2020-05-31 16:33:03 +0545

    Remove client id and session secret variable from constants

--- utils/constants.js
+++ utils/constants.js
@@ -4,8 +4,6 @@ dotenv.config({ path: ".env" });
 
 const constants = {
   mongoURI: process.env.MONGO_URI,
-  sessionSecret: process.env.SESSION_SECRET,
-  clientId: process.env.CLIENT_ID,
 };
 
 module.exports = constants;
